84e54c03c92e6632d27395dd530f3eb73cbc00d4
Leo <leonardorurrutia@gmail.com>
2021-03-13 17:02:04 -0800

    Updated code to suit our new 6 labels.

--- cannabis_image_model.py
+++ cannabis_image_model.py
@@ -3,4 +3,4 @@ from imageai.Classification.Custom import ClassificationModelTrainer
 model_trainer = ClassificationModelTrainer()
 model_trainer.setModelTypeAsResNet50()
 model_trainer.setDataDirectory("cannabis_checker")
-model_trainer.trainModel(num_objects=2, num_experiments=10, enhance_data=True, batch_size=32, show_network_summary=True)
+model_trainer.trainModel(num_objects=6, num_experiments=10, enhance_data=True, batch_size=32, show_network_summary=True)
--- image_recognition.py
+++ image_recognition.py
@@ -5,26 +5,21 @@ execution_path = os.getcwd()
 
 prediction = CustomImageClassification()
 prediction.setModelTypeAsResNet50()
+
+#set ModelPath to desired model. Copy the filename of the model and paste it after /models/
 prediction.setModelPath("cannabis_checker/models/model_ex-008_acc-0.871136.h5")
 prediction.setJsonPath("cannabis_checker/json/model_class.json")
-prediction.loadModel(num_objects=2)
-
-# for filename in os.listdir(directory):
-#     if filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".jpeg"): 
-         
-#         continue
-#     else:
-#         continue
+prediction.loadModel(num_objects=6)
 
 
-directory_in_str = "cannabis_checker/test/not_cannabis"
+directory_to_test = "cannabis_checker/test/not_cannabis"
 
-directory = os.fsencode(directory_in_str)
+directory = os.fsencode(directory_to_test)
     
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
-    if filename.endswith(".jpg") or filename.endswith(".jpeg"): 
-        predictions, probabilities = prediction.predictImage(f"{directory_in_str}/{filename}", result_count=1)
+    if filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".jfif") or filename.endswith(".png"): 
+        predictions, probabilities = prediction.predictImage(f"{directory_to_test}/{filename}", result_count=3)
 
         for eachPrediction, eachProbability in zip(predictions, probabilities):
             print(eachPrediction , " : " , eachProbability)
@@ -33,6 +28,3 @@ for file in os.listdir(directory):
     else:
         continue
 
-
-# for eachPrediction, eachProbability in zip(predictions, probabilities):
-#     print(eachPrediction , " : " , eachProbability)
